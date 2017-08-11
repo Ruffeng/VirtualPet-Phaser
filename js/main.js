f3b6515e1adb3078b7d969dca074d51fbab4c5f5
Ruffeng <ruben.suet@gmail.com>
2017-08-11 17:12:30 +0200

    Added dragabble pet and some specific events for all items

--- js/main.js
+++ js/main.js
@@ -25,6 +25,14 @@ var GameState = {
   },
   //executed after everything is loaded
   create: function() {
+    this.setDefaults();
+
+  },
+
+
+
+
+  setDefaults: function() {
     this.printBackground();
     this.printPet();
     this.printApple();
@@ -32,11 +40,6 @@ var GameState = {
     this.printToy();
     this.printRotate();
   },
-
-
-
-
-
   printBackground: function() {
    this.background = this.game.add.sprite(0, 0, 'backyard');
    return this.background
@@ -46,18 +49,45 @@ var GameState = {
     this.pet.anchor.setTo(0.5);
     // Custom properties
     this.pet.customParams = {health: 100, fun: 100};
+
+    // Make the pet draggable
+    // Let an object to have inputs
+    this.pet.inputEnabled = true;
+    this.pet.input.enableDrag();
   },
   printApple: function() {
     this.apple = this.game.add.sprite(72,570,'apple');
+    this.apple.anchor.setTo(0.5);
+    this.apple.inputEnabled = true;
+    this.apple.customParams = {health: 20};
+    this.apple.events.onInputDown.add(this.pickItem, this);
   },
   printCandy: function() {
     this.candy = this.game.add.sprite(144,570,'candy');
+    this.candy.anchor.setTo(0.5);
+    this.candy.inputEnabled = true;
+    this.candy.customParams = {health: -10, fun: 10};
+    this.candy.events.onInputDown.add(this.pickItem, this);
   },
   printToy: function() {
     this.toy = this.game.add.sprite(216,570,'toy');
+    this.toy.anchor.setTo(0.5);
+    this.toy.inputEnabled = true;
+    this.toy.customParams = {fun: 20};
+    this.toy.events.onInputDown.add(this.pickItem, this);
   },
   printRotate: function() {
     this.rotate = this.game.add.sprite(288,570,'rotate');
+    this.rotate.anchor.setTo(0.5);
+    this.rotate.inputEnabled = true;
+    this.rotate.events.onInputDown.add(this.rotatePet, this);
+  },
+  pickItem: function(sprite, event) {
+    console.log('pick item');
+    console.log(this);
+  },
+  rotatePet: function(sprite, event) {
+    console.log('rotate item');
   },
 
 };
